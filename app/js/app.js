c3d9c3f507a48af6cafc9a716ccdea372b416a5a
Dirk Baumann <baumanndirk@gmx.net>
2015-10-19 21:41:17 +0200

    Added Login and simple output list

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,13 @@
 'use strict';
-
+var username = "";
+var password = "";
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-      $routeProvider.
-          when('/showList', {
+angular
+    .module('myApp', ['ngRoute','myApp.version','base64'])
+    .config(['$routeProvider', function($routeProvider) {
+
+        $routeProvider.
+            when('/showList', {
             templateUrl: 'templates/showList.html'
           })
           .when('/login', {
@@ -16,7 +16,43 @@ config(['$routeProvider', function($routeProvider) {
           .otherwise({
             redirectTo: '/login'
           });
-}]);
+    }])
+    .controller('loginController', [
+         '$scope', '$http',
+        function($scope, $http) {
+            $scope.userName = username;
+            $scope.password = password;
+            $scope.login = function ( ) {
+                $http({
+                    method: 'GET',
+                    url: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/credential/check/' + $scope.UserName + '/' + $scope.Password
+                }).then(function successCallback(response) {
+                    if (response.data) {
+                        username = $scope.UserName;
+                        password = $scope.Password;
+                    }
+                }, function errorCallback(response) {
+                    console.log(response.data)
+                });
+            };
+        }])
+    .controller('listController', [
+        '$base64', '$scope', '$http',
+        function($base64, $scope, $http){
+            $http.defaults.headers.common.Authorization = 'Basic ' + $base64.encode(username + ':' + password);
+            $http({
+                method: 'GET',
+                url: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/joboffer/offers/joboffer/0/-1',
+                respondType: 'json'
+            }).then(function successCallback(response) {
+                var obj = response.data;
+                $scope.jobs = obj.offers;
+            }, function errorCallback(response) {
+                console.log(response)
+            });
+        }
+]);
+
 
 // TODO fix bugs - Keep dropdown menu open until clicked again (add to <li class="dropdown">)
 /*$('.dropdown.keep-open').on({
