384051e8e5a2e0a048525be4079cc7457fc51029
Julia Vogel <julia.vogel.kehl@web.de>
2015-10-22 00:04:57 +0200

    Improved login
    - added Credentials factory for saving credentials across templates
    - used encrypted password for credentials check

--- app/js/app.js
+++ app/js/app.js
@@ -1,62 +1,84 @@
 'use strict';
-var username = "";
-var password = "";
 // Declare app level module which depends on views, and components
 angular
     .module('myApp', ['ngRoute','myApp.version','base64'])
+    .factory('Credentials', function() {
+        var Credentials = {
+            username: '',
+            password: '',
+            encryptedpassword: ''
+        };
+        return Credentials;
+    })
     .config(['$routeProvider', function($routeProvider) {
 
-        $routeProvider.
-            when('/showList', {
-            templateUrl: 'templates/showList.html'
-          })
-          .when('/login', {
-            templateUrl: 'templates/login.html'
-          })
-          .otherwise({
-            redirectTo: '/login'
-          });
-    }])
-    .controller('loginController', [
-         '$scope', '$http',
-        function($scope, $http) {
-            $scope.userName = username;
-            $scope.password = password;
-            $scope.login = function ( ) {
-                $http({
-                    method: 'GET',
-                    url: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/credential/check/' + $scope.UserName + '/' + $scope.Password
-                }).then(function successCallback(response) {
-                    if (response.data) {
-                        username = $scope.UserName;
-                        password = $scope.Password;
-                    }
-                }, function errorCallback(response) {
-                    console.log(response.data)
-                });
-            };
-        }])
-    .controller('listController', [
-        '$base64', '$scope', '$http',
-        function($base64, $scope, $http){
-            $http.defaults.headers.common.Authorization = 'Basic ' + $base64.encode(username + ':' + password);
-            $http({
-                method: 'GET',
-                url: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/joboffer/offers/joboffer/0/-1',
-                respondType: 'json'
-            }).then(function successCallback(response) {
-                var obj = response.data;
-                $scope.jobs = obj.offers;
-            }, function errorCallback(response) {
-                console.log(response)
+        $routeProvider
+            .when('/showList', {
+                templateUrl: 'templates/showList.html'
+            })
+            .when('/login', {
+                templateUrl: 'templates/login.html'
+            })
+            .otherwise({
+                redirectTo: '/login'
             });
-        }
-]);
+    }])
+    .controller('loginController', function($scope, $http, $location, $base64, Credentials) {
+        $scope.login = function () {
+            Credentials.username = '';
+            Credentials.password = '';
+            Credentials.encryptedpassword = '';
 
+            // Encrypt password
+            var config = {
+                method: 'GET',
+                url: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/credential/encryptedpassword',
+                headers: {
+                    'Authorization': 'Basic ' + $base64.encode($scope.username + ':' + $scope.password)
+                },
+                transformResponse: [function (data) {
+                    // do nothing, simply pass the encrypted password
+                    return data;
+                }]
+            };
+            $http(config)
+                .then(function successCallback(response) {
+                    Credentials.encryptedpassword = response;
 
-// TODO fix bugs - Keep dropdown menu open until clicked again (add to <li class="dropdown">)
-/*$('.dropdown.keep-open').on({
-  "shown.bs.dropdown": function() { this.closable = false; },
-  "click":             function() { this.closable = false; },
-  "hide.bs.dropdown":  function() { return this.closable; }
-});*/
+                    // Check credentials
+                    var config = {
+                        method: 'GET',
+                        url: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/credential/check/' + $scope.username + '/' + Credentials.encryptedpassword
+                    }
+                    $http(config)
+                        .then(function successCallback(response) {
+                            Credentials.username = $scope.username;
+                            Credentials.password = $scope.password;
+                            $location.path('/showList');
+                        }, function errorCallback(response) {
+                            console.log(response.data);
+                            alert('Login fehlgeschlagen.');
+                        });
+                }, function errorCallback(response) {
+                    console.log(response);
+                    alert('Login fehlgeschlagen.');
+                });
+        };
+    })
+    .controller('listController', function($base64, $scope, $http, Credentials){
+        var config = {
+            method: 'GET',
+            url: 'https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST/joboffer/offers/joboffer/0/-1',
+            headers: {
+                'Authorization': 'Basic ' + $base64.encode(Credentials.username + ':' + Credentials.password)
+            },
+            respondType: 'json'
+        };
+        $http(config).then(function successCallback(response) {
+            var obj = response.data;
+            $scope.jobs = obj.offers;
+        }, function errorCallback(response) {
+            console.log(response)
+            alert('Anzeige fehlgeschlagen.');
+        });
+    });
